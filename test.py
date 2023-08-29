12b7ce7eb6b62719b78cd7a1b2f4cef81705922c
Mayank Patel <77495279+MayankPatel2602@users.noreply.github.com>
2023-08-29 20:51:04 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from new import SpacecraftNavigator
 
 class TestSpacecraftNavigator(unittest.TestCase):
 
-    def test_full(self):
+    def test_1(self):
         navigator = SpacecraftNavigator()
         initial_position = (10,15,40)
         initial_direction = "N"
@@ -16,6 +16,16 @@ class TestSpacecraftNavigator(unittest.TestCase):
         self.assertEqual(final_position, (10,16,39))
         self.assertEqual(final_direction, "N")
 
+    def test_2(self):
+        navigator = SpacecraftNavigator()
+        initial_position = (2,4,-10)
+        initial_direction = "E"
+        commands = "udffrflb"
+
+        final_position, final_direction = navigator.navigate(commands, initial_position, initial_direction)
+
+        self.assertEqual(final_position, (3,3,-10))
+        self.assertEqual(final_direction, "E")
 
 if __name__ == "__main__":
     unittest.main()
